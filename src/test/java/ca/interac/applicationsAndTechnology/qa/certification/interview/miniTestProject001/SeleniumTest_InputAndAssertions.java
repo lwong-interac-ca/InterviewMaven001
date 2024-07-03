7471a6b3156c880a80d2ce220ce913764de36b34
Lawrence Wong <lwong@interac.ca>
2024-07-03 11:08:57 -0400

    added first set of questions

---
+++ src/test/java/ca/interac/applicationsAndTechnology/qa/certification/interview/miniTestProject001/SeleniumTest_InputAndAssertions.java
@@ -0,0 +1,68 @@
+package ca.interac.applicationsAndTechnology.qa.certification.interview.miniTestProject001;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+/**
+ * Using Selenium Webdriver, do the following task on the Interac website
+ * (https://www.interac.ca):
+ * 
+ * Create a script to ensure that searching for the term "transcendentalism"
+ * yields no result.
+ * 
+ * To aid in rapid root-cause analysis of test failures, logging of the
+ * appropriate level is encouraged. Screenshots may also be taken.
+ * 
+ * The test case should reasonably favour false-positive (thus, the logging
+ * requirement above) over false-negative.
+ */
+public class SeleniumTest_InputAndAssertions {
+	
+	private WebDriver webdriver = null;
+	private Logger logger = LogManager.getFormatterLogger();
+	
+	@BeforeClass
+	public void beforeClass() {
+		logger.atDebug().log("Test class starting %s", SeleniumTest_InputAndAssertions.class.getCanonicalName());
+	}
+
+	@AfterClass
+	public void afterClass() {
+		logger.atDebug().log("Test class ended %s", SeleniumTest_InputAndAssertions.class.getCanonicalName());
+	}
+
+	@BeforeMethod
+	public void beforeMethod() {
+		
+	}
+
+	@AfterMethod
+	public void afterMethod() {
+		
+	}
+	
+	@BeforeTest
+	public void beforeTest() {
+		
+	}
+
+	@AfterTest
+	public void afterTest() {
+		
+	}
+	
+	@Test
+	public void test() {
+		
+	}
+	
+
+}
